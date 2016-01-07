f1713ffc9be678bc990bb7c8f43201c7e8b508c0
cyz.aoj <cyz.aoj@gmail.com>
2016-01-07 08:34:40 +0800

    项目脚手架-增加对controller异常处理

--- web-server/src/main/java/com/tuicr/scaffold/controller/ViewResolverAdvice.java
+++ web-server/src/main/java/com/tuicr/scaffold/controller/ViewResolverAdvice.java
@@ -1,25 +1,13 @@
 package com.tuicr.scaffold.controller;
 
-import com.tuicr.scaffold.data.DataModelResult;
-import com.tuicr.scaffold.data.StateCode;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.support.RequestContextUtils;
-
-import javax.servlet.http.HttpServletRequest;
-import java.sql.SQLException;
 
 /**
  * restful接口异常处理
